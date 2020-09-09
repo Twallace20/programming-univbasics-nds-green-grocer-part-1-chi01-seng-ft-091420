a4a78ef3faa5e12e6afd565f9cbbccdc7c80a014
Tyler Wallace <tywallace2017@gmail.com>
2020-09-09 00:13:11 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,6 +3,14 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
 
+grocery_shelf = [
+  { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
+  { :item => "CANNED CORN", :price => 2.50, :clearance => false },
+  { :item => "SALSA", :price => 1.50, :clearance => false },
+  { :item => "TORTILLAS", :price => 2.00, :clearance => false },
+  { :item => "HOT SAUCE", :price => 1.75, :clearance => false }
+]
+find_item_by_name_in_collection[:item, "WINE"]
 end
 
 def consolidate_cart(cart)
@@ -11,7 +19,13 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-end
+unconsolidated_cart = [
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "KALE", :price => 3.00, :clearance => false}
+]
 
+consolidate_cart[unconsolidated_cart]
+end
 
   
